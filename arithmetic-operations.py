a68a5dba627d32cb4d46259a1fbf9a5944a4d6b4
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-16 18:09:36 +0100

    add first module to test

---
+++ arithmetic-operations.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+
+x = 12
+y = 5
+
+print(add(x,y), subtract(x,y), multiply(x,y), divide(x,y))
