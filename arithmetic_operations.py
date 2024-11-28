4a66eb6ced429ecbe02a9ebe3e5b6c83d8fcde87
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-29 00:44:40 +0100

    raise error when arguments arent numbers

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -1,4 +1,6 @@
 def add(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Unsupported types for add()")
     return a + b
 
 def subtract(a, b):
@@ -14,4 +16,4 @@ def divide(a, b):
 x = 12
 y = 5
 
-print(add(x,y), subtract(x,y), multiply(x,y), divide(x,y))
+# print(add(x,y), subtract(x,y), multiply(x,y), divide(x,y))
