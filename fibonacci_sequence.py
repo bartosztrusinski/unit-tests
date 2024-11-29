ce7a570a1f6c778615ae136af02598f727ba4bc7
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-29 14:14:58 +0100

    add input safety

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,4 +1,9 @@
 def fib_iter(n):
+    if not isinstance(n, int):
+        raise TypeError("Input must be an integer")
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer")
+    
     if n < 2:
         return n
     a, b = 0, 1
@@ -7,13 +12,15 @@ def fib_iter(n):
     return b
 
 def fib_rec(n):
+    if not isinstance(n, int):
+        raise TypeError("Input must be an integer")
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer")
+    
     if n < 2:
         return n
     return fib_rec(n - 1) + fib_rec(n - 2)
 
-list = [0, 1, 2, 3, 4, 5, 12, 30]
-res_iter = [fib_iter(n) for n in list]
-res_rec = [fib_rec(n) for n in list]
-
-print(res_iter)
-print(res_rec)
+# list = [0, 1, 2, 3, 4, 5, 12, 30]
+# print([fib_iter(n) for n in list])
+# print([fib_rec(n) for n in list])
