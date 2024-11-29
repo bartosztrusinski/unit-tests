303b05244a1284ae75df817a9edecf10d43ae6dd
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-29 13:22:34 +0100

    raise invalid input error in all functions

--- arithmetic_operations.py
+++ arithmetic_operations.py
@@ -4,16 +4,21 @@ def add(a, b):
     return a + b
 
 def subtract(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Unsupported types for subtract()")
     return a - b
 
 def multiply(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Unsupported types for multiply()")
     return a * b
 
 def divide(a, b):
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("Unsupported types for divide()")
     return a / b
 
 
-x = 12
-y = 5
-
+# x = 12
+# y = 5
 # print(add(x,y), subtract(x,y), multiply(x,y), divide(x,y))
