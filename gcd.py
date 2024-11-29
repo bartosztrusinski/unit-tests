849733e1bf35ba2063f83a9bdac5eb6b065d74a3
Bartosz Trusiński <bartosztrusinski@gmail.com>
2024-11-29 14:36:03 +0100

    add type safety

--- gcd.py
+++ gcd.py
@@ -1,13 +1,22 @@
 def gcd_iter(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise TypeError("Both inputs must be integers.")
+    if a < 0 or b < 0:
+        raise ValueError("Both inputs must be non-negative.")
+    
     while b:
         a, b = b, a % b
     return a
 
 def gcd_rec(a, b):
+    if not isinstance(a, int) or not isinstance(b, int):
+        raise TypeError("Both inputs must be integers.")
+    if a < 0 or b < 0:
+        raise ValueError("Both inputs must be non-negative.")
+    
     if b == 0:
         return a
     return gcd_rec(b, a % b)
 
-x, y = 20, 8
-
-print(gcd_iter(x, y), gcd_rec(x, y))
+# x, y = 20, 8
+# print(gcd_iter(x, y), gcd_rec(x, y))
